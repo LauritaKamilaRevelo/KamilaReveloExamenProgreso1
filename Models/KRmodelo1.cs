06e3eafb5cc4ef2ba3be4f12a157f8fa5cf849dd
LauritaKamilaRevelo <laurita.revelo@udla.edu.ec>
2024-10-28 09:46:35 -0500

    clase KRmodelo1

---
+++ Models/KRmodelo1.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KamilaReveloExamenProgreso1.Models
+{
+    public class KRmodelo1
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Nombre { get; set; }
+        public bool WithTea { get; set; }
+        [Range(0.1, 99.99)]
+        public decimal Precio { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaRegristo { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaFinal { get; set; }
+    }
+}
